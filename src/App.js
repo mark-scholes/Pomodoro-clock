c7ad8a5e305a20338d0d51a5fc7c4f941b5fee1f
mark-scholes <innominatus15@outlook.com>
2020-04-13 02:55:41 +0100

    First commit after inital setup after create-react-app and housekeeping

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="title">
+        <p>POMODORO CLOCK</p>
+      </header>
+      <body>
+        <div className="clock">Clock</div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
